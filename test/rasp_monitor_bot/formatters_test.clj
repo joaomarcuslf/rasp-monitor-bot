e89c52209f16de9f9e518ba13cdbd1320f439b98
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2017-03-11 19:21:47 -0300

    Added tests for formatters

---
+++ test/rasp_monitor_bot/formatters_test.clj
@@ -0,0 +1,25 @@
+(ns rasp-monitor-bot.formatters-test
+  (:require [clojure.test :refer :all]
+    [rasp-monitor-bot.formatters :as formatters]))
+(def help
+  [{ :name "help", :description "test" }])
+
+(deftest format-help-test
+  (testing "format-help, Should format an array of help."
+    (is (= ["help - test"] (formatters/format-help help)))))
+
+(deftest format-command-t1
+  (testing "format-command, Should format an command, removing sudo reference."
+    (is (= "rm" (formatters/format-command "sudo rm")))))
+
+(deftest format-command-t2
+  (testing "format-command, Should format an command, removing /command reference."
+    (is (= "rm" (formatters/format-command "/command rm")))))
+
+(deftest format-output-t1
+  (testing "format-output, Should format an command output, when a err is given."
+    (is (= "test" (formatters/format-output { :err "test" })))))
+
+(deftest format-output-t2
+  (testing "format-output, Should format an command output, when a out is given."
+    (is (= "test" (formatters/format-output { :out "test" })))))
